2f1af403b422fcf841664a8aba11fb075ba74271
Hari Govind <grhari1998@gmail.com>
2019-09-20 22:07:58 -0400

    almost done front page

--- MemoryGame/MemoryGame/ViewController.swift
+++ MemoryGame/MemoryGame/ViewController.swift
@@ -8,13 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let possibleMatches = [2,3,4]
+    var selectedMatches = 2
+    var gridMin = 0
+    var gridMax = 1
+    
+    let gridForTwo = [4,5]
+    let gridForThree = [6,5]
+    let grodForFour = [5,8]
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return possibleMatches.count
+    }
+    
+    internal func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        setMaxAndMin(matches: possibleMatches[row])
+        return String(possibleMatches[row])
+    }
+    func setMaxAndMin(matches: Int){
+        
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        gridPicker.delegate = self
+        gridPicker.dataSource = self
+        stepper.wraps = true
+        stepper.autorepeat = true
+        stepper.minimumValue = Double(gridMin);
+        stepper.maximumValue = Double(gridMax);
     }
-
-
+    
+    @IBAction func gridSizeStepper(_ sender: UIStepper) {
+        xGridValue.text = String(gridForTwo[Int(sender.value)])
+    }
+    
+    @IBOutlet weak var gridPicker: UIPickerView!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var tstLabel: UILabel!
+    @IBOutlet weak var xGridValue: UILabel!
+    @IBOutlet weak var yGridValue: UILabel!
 }
 
